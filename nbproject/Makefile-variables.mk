4c0b69c3e31729fa93ea6a988ca6d338a5200e7f
Fer18313 <80397009+Fer18313@users.noreply.github.com>
2021-08-18 14:34:47 -0600

    new change

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=LAB5_temp.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/LAB5_temp.X.production.hex
+CND_ARTIFACT_NAME_default=lab5.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lab5.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=lab5temp.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab5temp.x.tar
+CND_PACKAGE_NAME_default=lab5.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab5.tar
